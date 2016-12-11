4787b7c2aee0f16d70800209c200dbed5c1765e3
paolopiaggio <paolo.piaggesi@gmail.com>
2016-12-11 14:34:59 +0000

    added gulp-nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const nodemon = require('gulp-nodemon')
 
 gulp.task('mocha-tests', () => {
   return gulp.src('tests/**/*Test.js')
     .pipe(mocha({reporter: 'spec', bail: true}))
     .once('error', (err) => {
       console.log(err);
-      process.exit(1); 
+      process.exit(1);
     })
     .once('end', () => {
       process.exit();
     })
 });
 
-gulp.task('default', ['mocha-tests']);
+gulp.task('dev-mode', () => {
+  nodemon({script: './api/app.js'})
+});
+
+gulp.task('default', ['dev-mode'])
+gulp.task('tests', ['mocha-tests']);
